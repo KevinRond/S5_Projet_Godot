b4718d872dc148bd07099cef0f1d4d2c0ff16111
Kevin <ronk2602@usherbrooke.ca>
2024-10-22 13:03:49 -0400

    ptit fix pas important

--- scenes/Pi_car/scripts/Pi_car.gd
+++ scenes/Pi_car/scripts/Pi_car.gd
@@ -4,13 +4,6 @@ var nfsm = 0
 var speed = 0
 var capteurs_SL = []
 
-func check_capteurs_SL():
-	for i in range(capteurs_SL.size()):
-		if capteurs_SL[i] == true:
-			print("Capteurs %d detecte la ligne" % (i+1))
-		else:
-			print("Capteurs %d ne detecte pas la ligne" % (i+1))
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	nfsm = $"../NetworkFSM"
@@ -31,11 +24,18 @@ func _process(delta):
 		speed = 0
 	translate(Vector3(-delta * speed, 0, 0))
 	
-	# Loop pour debug les capteurs
-	check_capteurs_SL()
 
 func _physics_process(delta):
 	pass
+
+
+# Fonction utilitaire pour vérifier ce que les capteurs du suiveur de ligne voient
+func check_capteurs_SL():
+	for i in range(capteurs_SL.size()):
+		if capteurs_SL[i] == true:
+			print("Capteurs %d detecte la ligne" % (i+1))
+		else:
+			print("Capteurs %d ne detecte pas la ligne" % (i+1))
 
 
 func _on_capteur_1_area_entered(area):
