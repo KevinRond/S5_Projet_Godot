48ac6d4b94233bbd981c5f33a93e4b1ddc8c1e44
emilejt <emilejacquestighe@gmail.com>
2024-11-27 17:07:17 -0500

    test

--- scenes/Pi_car/scripts/Pi_car_remote.gd
+++ scenes/Pi_car/scripts/Pi_car_remote.gd
@@ -322,8 +322,8 @@ func treat_info(delta, capteurs, distance):
 				rotation = CENTRE - AVOID_SIDE*AIDE_COURBURE
 			
 			if capteurs[0] or capteurs[1] or capteurs[2] or capteurs[3] or capteurs[4]:
-				if speed < V_MAX: 
-					speed = 0.08
+				#if speed < V_MAX: 
+					#speed = 0.04
 				state = State.following_line
 				
 		State.waiting:
