2436f652a6670425c5194a180ac57044bfb66230
Kevin <ronk2602@usherbrooke.ca>
2024-11-27 15:26:16 -0500

    LETS GOOOOO

--- scenes/Pi_car/scripts/Pi_car_remote.gd
+++ scenes/Pi_car/scripts/Pi_car_remote.gd
@@ -50,8 +50,8 @@ const GAUCHE = -30
 const DROITE = 30
 const AIDE_COURBURE = 15
 
-const AVOID_TIME = 2
-const RETURN_TIME = 2
+const AVOID_TIME = 1.5
+const RETURN_TIME = 1
 
 var nfsm = 0
 var speed = 0
