a2a3f3a18ee0a57f35eda49bd74027b79e78b557
Kevin <ronk2602@usherbrooke.ca>
2024-11-27 13:29:16 -0500

    2 ou 3

--- scenes/Pi_car/scripts/Pi_car_remote.gd
+++ scenes/Pi_car/scripts/Pi_car_remote.gd
@@ -276,7 +276,7 @@ func treat_info(delta, capteurs):
 				else:
 					if speed < 0:
 						speed += ACCELERATION * delta
-			if utils.finish_line_detected(capteurs) and line_passed >= 2:
+			if utils.finish_line_detected(capteurs) and line_passed > 2:
 				print("RETOURNE DANS FOLL")
 				state = State.following_line
 
