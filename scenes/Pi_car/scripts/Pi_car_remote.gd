9aa9186207f091b8cbcbf60da294d39d468bcc2f
ParciIllusion <104928000+Parcillusion@users.noreply.github.com>
2024-11-27 12:10:38 -0500

    timer exagere

--- scenes/Pi_car/scripts/Pi_car_remote.gd
+++ scenes/Pi_car/scripts/Pi_car_remote.gd
@@ -50,8 +50,8 @@ const GAUCHE = -30
 const DROITE = 30
 const AIDE_COURBURE = 15
 
-const AVOID_TIME = 1
-const RETURN_TIME = 0.6
+const AVOID_TIME = 0.6
+const RETURN_TIME = 1
 
 var nfsm = 0
 var speed = 0
