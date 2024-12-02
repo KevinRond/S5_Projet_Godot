938e51f4afd0da08137284bc6d380644bca90195
Kevin <ronk2602@usherbrooke.ca>
2024-12-02 13:46:16 -0500

    timer avant de cramper les roues

--- scenes/Pi_car/scripts/Pi_car_remote.gd
+++ scenes/Pi_car/scripts/Pi_car_remote.gd
@@ -171,9 +171,9 @@ func suivre_ligne(delta, speed, capteurs):
 	if !utils.line_detected(capteurs):
 		timer_retrouver_ligne += delta * 10
 		last_direction = movement_array.check_last_rotation()
-		if last_direction > 0:
+		if last_direction > 0 and timer_retrouver_ligne > 0.25:
 			new_rotation = 41
-		if last_direction < 0:
+		if last_direction < 0 and timer_retrouver_ligne > 0.25:
 			new_rotation = -41
 		if timer_retrouver_ligne >= 1.25:
 			if !utils.line_detected(capteurs):
