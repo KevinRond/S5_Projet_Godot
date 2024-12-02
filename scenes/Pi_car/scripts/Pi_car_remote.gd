ca0717852b1485cb1f091dd7dfcd15ef51a8a797
Kevin <ronk2602@usherbrooke.ca>
2024-12-02 14:07:37 -0500

    GG!

--- scenes/Pi_car/scripts/Pi_car_remote.gd
+++ scenes/Pi_car/scripts/Pi_car_remote.gd
@@ -172,10 +172,10 @@ func suivre_ligne(delta, speed, capteurs):
 		timer_retrouver_ligne += delta * 10
 		last_direction = movement_array.check_last_rotation()
 		new_rotation = last_direction
-		#if last_direction > 0 and timer_retrouver_ligne > 0.5:
-			#new_rotation = 41
-		#if last_direction < 0 and timer_retrouver_ligne > 0.5:
-			#new_rotation = -41
+		if last_direction > 0 and timer_retrouver_ligne > 0.5:
+			new_rotation = 41
+		if last_direction < 0 and timer_retrouver_ligne > 0.5:
+			new_rotation = -41
 		if timer_retrouver_ligne >= 1.25:
 			if !utils.line_detected(capteurs):
 				new_state = State.find_line
