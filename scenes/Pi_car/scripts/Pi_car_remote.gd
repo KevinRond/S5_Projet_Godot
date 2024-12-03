6c1dd9d184574b5d42d9bd767750e371191b56db
emilejt <emilejacquestighe@gmail.com>
2024-12-03 18:40:18 -0500

    dernier commit stait un troll hihi lui il lke fait

--- scenes/Pi_car/scripts/Pi_car_remote.gd
+++ scenes/Pi_car/scripts/Pi_car_remote.gd
@@ -297,13 +297,13 @@ func treat_info(delta, capteurs, robot_state):
 
 		State.find_line:
 			if utils.check_center_sensors(capteurs):
-				if speed < V_MAX:
-					speed =0.00
-				#if speed < -REAL_V_MIN:
-					#speed += 2*ACCELERATION*delta
-				#else:
-					#state = State.following_line
-				state = State.following_line
+				#if speed < V_MAX:
+					#speed =0.00
+				if speed < -REAL_V_MIN:
+					speed += 2*ACCELERATION*delta
+				else:
+					state = State.following_line
+				#state = State.following_line
 				
 			if speed > -V_MAX:
 				speed -= ACCELERATION * 2 * delta
